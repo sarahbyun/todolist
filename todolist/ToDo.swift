34b7204d532e4e03e4e3de53cfec5ba957f58182
sarahbyun <sbyun1029@gmail.com>
2022-06-28 08:35:59 -0700

    Initial Commit

---
+++ todolist/ToDo.swift
@@ -0,0 +1,13 @@
+//
+//  ToDo.swift
+//  todolist
+//
+//  Created by Sarah Byun on 6/27/22.
+//
+
+import UIKit
+
+class ToDo {
+    var name = ""
+    var important = false
+}
---
+++ todolist/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  todolist
+//
+//  Created by Sarah Byun on 6/27/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
